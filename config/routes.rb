1be2c73e311c0677f2a0196746adde429790c4e8
Zuza Zuber <zuberzuzanna@gmail.com>
2018-08-10 11:25:20 +0200

    Root defined

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
-  resources :cocktails, only: [:index, :show, :new, :create] do
+
+  root to: "cocktails#index"
+  resources :cocktails, only: [ :show, :new, :create] do
     resources :doses, only: [:new, :create]
   end
 
